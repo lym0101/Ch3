b7c82e592a0613169e5ed378f213c39bf8bffab4
Eric Li <65319100+lym0101@users.noreply.github.com>
2026-09-17 14:19:01 -0700

    Update GuessMyNumber.java
    Added user input for guessing the number and displayed the difference from the actual number.

--- GuessMyNumber.java
+++ GuessMyNumber.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessMyNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int number = random.nextInt(100) + 1; // Random number between 1 and 100
-        //int guess;
-        //int attempts = 0;
+        int number = random.nextInt(100) + 1; 
+        int guess = scanner.nextInt();
+        
         System.out.println("Welcome to the Guess My Number game!");
         System.out.println("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?");
+        
         System.out.println("Type a Number: ");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Your guess is: " + number);
+        
+        System.out.println("Your guess is: " + guess);
         System.out.println("The Number was: " + number);
+
+        int amtOff = Math.abs(number-guess);
+        System.out.println("You were off by: " + amtOff);
 }
