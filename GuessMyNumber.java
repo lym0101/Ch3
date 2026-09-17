6c9dbee205c5d8c2922f1d5307372e6414ae6a9e
Eric Li <65319100+lym0101@users.noreply.github.com>
2026-09-17 14:25:52 -0700

    Add files via upload

--- GuessMyNumber.java
+++ GuessMyNumber.java
@@ -5,17 +5,19 @@ public class GuessMyNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int number = random.nextInt(100) + 1; 
-        int guess = scanner.nextInt();
         
-        System.out.println("Welcome to the Guess My Number game!");
+        System.out.println("Welcome to the Guess My Number game! \n");
+        
+        int number = random.nextInt(100) + 1; 
         System.out.println("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?");
         
-        System.out.println("Type a Number: ");
+        System.out.print("Type a Number: ");
         
+        int guess = scanner.nextInt();
         System.out.println("Your guess is: " + guess);
         System.out.println("The Number was: " + number);
 
         int amtOff = Math.abs(number-guess);
         System.out.println("You were off by: " + amtOff);
+	}
 }
